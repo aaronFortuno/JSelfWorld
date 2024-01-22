989d56fffabdea27add9af3d60b5f81fec62073e
aaronFortuno <aaron.fortuno@protonmail.com>
2024-01-22 07:07:56 +0100

    Adding buttons to add points and segments

--- js/math/graph.js
+++ js/math/graph.js
@@ -8,6 +8,34 @@ class Graph {
         this.points.push(point);
     }
 
+    containsPoint(point) {
+        return this.points.find((p) => p.equals(point));
+    }
+
+    tryAddPoint(point) {
+        if (!this.containsPoint(point)) {
+            this.addPoint(point);
+            return true;
+        }
+        return false;
+    }
+
+    addSegment(seg) {
+        this.segments.push(seg);
+    }
+
+    containsSegment(segment) {
+        return this.segments.find((s) => s.equals(segment));
+    }
+
+    tryAddSegment(segment) {
+        if (!this.containsSegment(segment) && !segment.p1.equals(segment.p2)) {
+            this.addSegment(segment);
+            return true;
+        }
+        return false;
+    }
+
     draw(context) {
         for (const seg of this.segments) {
             seg.draw(context);
--- js/primitives/segment.js
+++ js/primitives/segment.js
@@ -4,6 +4,14 @@ class Segment {
         this.p2 = p2;
     }
 
+    equals(segment) {
+        return this.includes(segment.p1) && this.includes(segment.p2);
+    }
+
+    includes(point) {
+        return this.p1.equals(point) || this.p2.equals(point);
+    }
+
     draw(context, width = 2, color = "black") {
         context.beginPath();
         context.lineWidth = width;
